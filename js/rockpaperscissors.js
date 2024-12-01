b3df16b21521068f1f61484d75d0537f11c3ee2b
doncarlmiko <konoha11061997@gmail.com>
2024-12-01 21:24:39 +0800

    Included comments to the rockpaperscissors.js file

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,20 +1,39 @@
-console.log('Hello world');
 
-function getComputerChoice(){
-    const Rock = 'Rock';
-    const Paper = 'Paper';
-    const Scissors = 'Scissors';
-    const RandomNumber = Math.floor(Math.random() * 3);
+    //Create a function getComputerChoice
+    function getComputerChoice(){
 
-    if (RandomNumber === 0){
-        return Rock;
-    }
-    else if (RandomNumber === 1){
-        return Paper;
-    }
-    else if (RandomNumber === 2){
-        return Scissors;
-    }
-}
+        //create a string variable Rock for function getComputerChoice
+        //put "Rock" in Rock
+        const Rock = 'Rock';
+        
+        //Create a string variable Paper for function getComputerChoice
+        //put "Paper" in Paper
+        const Paper = 'Paper';
 
+        //create a string variable Scissors for function getComputerChoice
+        //put "Scissors" in Scissors
+        const Scissors = 'Scissors';
+        //put Math.floor(Math.random() * 3) in RandomNumber
+
+        //create an integer variable RandomNumber for function getComputerChoice
+        //put Math.floor(Math.random() * 3) in RandomNumber
+        const RandomNumber = Math.floor(Math.random() * 3);
+
+        //If RandomNumber is equal to 0 return Rock
+        if (RandomNumber === 0){
+            return Rock;
+        }
+
+        //If RandomNumber is equal to 1 return Paper
+        else if (RandomNumber === 1){
+            return Paper;
+        }
+
+        //If RandomNumber is equal to 2 return Scissors
+        else if (RandomNumber === 2){
+            return Scissors;
+        }
+    }
+
+//print getComputerChoice
 console.log(getComputerChoice());
