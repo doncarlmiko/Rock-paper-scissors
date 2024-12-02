f20e73156a723cffd14039108ae726c158aa6956
doncarlmiko <konoha11061997@gmail.com>
2024-12-02 20:07:25 +0800

    added comments for each line of code. called the getHumanchoice() using the console.log

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -35,35 +35,50 @@
         }
     }
 
-    
+    //Create a function getHumanChoice()
     function getHumanChoice(){
+
+        //create a variable string userInput for function getHumanChoice()
+        //put prompt("Pick one - Rock, Paper, Scissors: ", "") to userInput
         const userInput = prompt("Pick one - Rock, Paper, Scissors: ", "");
 
+        //create a variable string Rock for function getHumanChoice()
+        //put "Rock" in Rock
         const Rock = 'Rock';
+
+        //create a variable string Paper for function getHumanChoice()
+        //put "Paper" in Paper
         const Paper = 'Paper';
+
+        //create a variable string Scissors for function getHumanChoice()
+        //put "Scissors" in Scissors
         const Scissors = 'Scissors';
 
+        //if userInput is equal to Rock, return Rock
         if (userInput.toLowerCase().includes(Rock.toLowerCase())){
             return Rock;
         }
+
+        //If userInput is equal to Paper, return Paper
         else if(userInput.toLowerCase().includes(Paper.toLowerCase())){
             return Paper;
         }
 
+        //If userInput is equal to Scissors, return Scissors
         else if(userInput.toLowerCase().includes(Scissors.toLowerCase())){
             return Scissors;
         }
     }
 
+//create a variable humanSelection
+//set humanSelection to result of calling getHumanChoice()
+const humanSelection = getHumanChoice();
 
+//print humanSelection
+console.log(humanSelection);
 
-
-
-
-
-
-
-//create a variable computerSelection set computerSelection to result of calling getComputerChoice()
+//create a variable computerSelection 
+//set computerSelection to result of calling getComputerChoice()
 const computerSelection = getComputerChoice();
 
 // print computerSelection
