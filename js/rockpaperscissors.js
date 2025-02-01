f30b9981e24e6f89004c90119dd7e7eac103b7be
doncarlmiko <konoha11061997@gmail.com>
2025-02-01 19:35:41 +0800

    Finished refactoring playRound function

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -160,45 +160,4 @@ function playRound(humanWinner, computerWinner, humanChoice){
      
 }
 
-//call playRound(humanSelection, computerSelection)
-//playRound(humanSelection, computerSelection);
-
-function playGame(){
-    let loopRound;
-    /*for(loopRound = 1; loopRound <= 5; loopRound++){
-        // Get new choices for each round
-
-        //create a variable humanSelection
-        //set humanSelection to result of calling getHumanChoice()
-        //const humanSelection = getHumanChoice();
-        const humanSelection = getHumanChoice();
-        if (humanSelection === null) {
-            // Exit the game if the user cancels
-            console.log("Game cancelled by the user.");
-            return;
-        }
-
-        //create a variable computerSelection 
-        //set computerSelection to result of calling getComputerChoice()
-        //const computerSelection = getComputerChoice();
-        const computerSelection = getComputerChoice();
-
-        //console.log(`Human chose: ${humanSelection}`);
-        //console.log(`Computer chose: ${computerSelection}`);
-
-        // Play a round and update scores
-        playRound(humanSelection, computerSelection);
-    }*/
-
-    console.log(`\nFinal Score: Human: ${humanScore}, Computer: ${computerScore}`);
-    if (humanScore > computerScore) {
-        console.log("Congratulations! You win the game!");
-    } else if (computerScore > humanScore) {
-        console.log("You lose! Better luck next time.");
-    } else {
-        console.log("It's a tie game!");
-    }
-}
-
-//playGame();
 
